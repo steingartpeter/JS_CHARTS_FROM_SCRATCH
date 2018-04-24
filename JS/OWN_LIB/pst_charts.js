38cb91c9d020af925b3f83eb23b4e4b8dfdbcdf3
steingartpeter <steingart.p2@gmail.com>
2018-04-24 17:48:19 +0200

    2018-04-24 001 AX07057 HOME PAKS2

--- JS/OWN_LIB/pst_charts.js
+++ JS/OWN_LIB/pst_charts.js
@@ -50,6 +50,7 @@ function FUNCTION_SETUP(){
 	PSTCG.ADD_TEXT = a0030;
 	PSTCG.ADD_BGGRID = a0040;
 	PSTCG.ADD_AXIS = a0050;
+	PSTCG.CRT_SCALE = a0060;
 }
 
 
@@ -110,10 +111,22 @@ function a0010(){
 	PSTCG.ADD_BGGRID(svg, {});
 	
 	//<nn>
-	// Hozzáadjuk a háttér hálót.
+	// Hozzáadjuk az x tengelyt.
 	//</nn>
 	PSTCG.ADD_AXIS(svg, {});
 	
+	var axPrm = {"align":"y-left"}
+	//<nn>
+	// Hozzáadjuk az y tengelyt.
+	//</nn>
+	PSTCG.ADD_AXIS(svg, axPrm);
+	
+	axPrm = {"align":"y-right"}
+	//<nn>
+	// Hozzáadjuk az y tengelyt.
+	//</nn>
+	PSTCG.ADD_AXIS(svg, axPrm);
+	var clsr = "";
 }
 
 function a0011(svgElmnt, txtId){
@@ -297,7 +310,7 @@ function a0040(svgElmnt, prmObj){
 		prmObj.lineColor = "#CDCDCD";
 	}
 	if(prmObj.xLnNr == undefined){
-		prmObj.xLnNr = 5;
+		prmObj.xLnNr = PSTCG_CNSTS.DEF_RECNR;
 	}
 	if(prmObj.yLnNr == undefined){
 		prmObj.yLnNr = prmObj.xLnNr;
@@ -405,20 +418,30 @@ function a0050(svgElmnt, prmObj){
 		prmObj = {};
 	}
 	if(prmObj.align == undefined){
-		prmObj.align = "x";
+		prmObj.align = "x-bottom";
 	}
 	if(prmObj.lineColor == undefined){
 		prmObj.lineColor = "#FF0505";
 	}
 	if(prmObj.xXlate == undefined){
-		prmObj.xXlate = PSTCG_CNSTS.DEFS.BASE_S_SVG_MRG_L;
+		if(prmObj.align == "y-right"){
+			prmObj.xXlate = PSTCG_CNSTS.DEFS.BASE_S_SVG_W - PSTCG_CNSTS.DEFS.BASE_S_SVG_MRG_R;
+		}else{
+			prmObj.xXlate = PSTCG_CNSTS.DEFS.BASE_S_SVG_MRG_L;
+		}
 	}
 	if(prmObj.yXlate == undefined){
-		prmObj.yXlate = PSTCG_CNSTS.DEFS.BASE_S_SVG_H-PSTCG_CNSTS.DEFS.BASE_S_SVG_MRG_B;
+		if(prmObj.align == "x-bottom"){
+			prmObj.yXlate = PSTCG_CNSTS.DEFS.BASE_S_SVG_H-PSTCG_CNSTS.DEFS.BASE_S_SVG_MRG_B;
+		}else{
+			prmObj.yXlate = PSTCG_CNSTS.DEFS.BASE_S_SVG_MRG_T;
+		}
 	}
 	if(prmObj.length == undefined){
-		if(prmObj.align == "x"){
+		if(prmObj.align == "x-bottom" || prmObj.align == "x-top"){
 			prmObj.length = PSTCG_CNSTS.DEFS.BASE_S_SVG_W - PSTCG_CNSTS.DEFS.BASE_S_SVG_MRG_L - PSTCG_CNSTS.DEFS.BASE_S_SVG_MRG_R;
+		}else if(prmObj.align == "y-left" || prmObj.align == "y-right"){
+			prmObj.length = PSTCG_CNSTS.DEFS.BASE_S_SVG_H - PSTCG_CNSTS.DEFS.BASE_S_SVG_MRG_T - PSTCG_CNSTS.DEFS.BASE_S_SVG_MRG_B;
 		}else{
 			prmObj.length = PSTCG_CNSTS.DEFS.BASE_S_SVG_H - PSTCG_CNSTS.DEFS.BASE_S_SVG_MRG_T - PSTCG_CNSTS.DEFS.BASE_S_SVG_MRG_B;
 		}
@@ -432,13 +455,13 @@ function a0050(svgElmnt, prmObj){
 		prmObj.ticks = {};
 	}
 	if(prmObj.ticks.tckNr == undefined){
-		prmObj.ticks.tckNr = 5;
+		prmObj.ticks.tckNr = PSTCG_CNSTS.DEF_RECNR;
 	}
 	if(prmObj.ticks.lineColor == undefined){
 		prmObj.ticks.lineColor = prmObj.lineColor;
 	}
 	if(prmObj.ticks.align == undefined){
-		if(prmObj.align == 'y'){
+		if(prmObj.align == 'y-left' || prmObj.align == 'y-right'){
 			prmObj.ticks.align = 'x';
 		}else{
 			prmObj.ticks.align = 'y';
@@ -447,6 +470,19 @@ function a0050(svgElmnt, prmObj){
 	if(prmObj.ticks.length == undefined){
 		//prmObj.ticks.length = prmObj.length/10;
 		prmObj.ticks.length = 4;
+	}
+	
+	//<nn>
+	// Ha nem jött tengely-felirat tömb akkor csinálunk :)
+	//</nn>
+	if(prmObj.tickText == undefined){
+		prmObj.tickText = [];
+		for(var ix1 = 1; ix1 <= prmObj.ticks.tckNr;ix1++){
+			prmObj.tickText[ix1-1] = ix1;
+		}
+	}
+	if(prmObj.showTickText == undefined){
+		prmObj.showTickText = true;
 	}
 	
 	//<nn>
@@ -473,10 +509,20 @@ function a0050(svgElmnt, prmObj){
 	//<nn>
 	// Különféleképpen kell kezelni az X, és az Y tengelyek értékeit.
 	//</nn>
-	if(prmObj.align == "x"){
+	if(prmObj.align == "x-bottom"){
 		ln.attr({
 			"x2":0 + prmObj.xXlate + prmObj.length,
 			"y2":0 + prmObj.yXlate
+		});
+	}else if(prmObj.align == "y-left"){
+		ln.attr({
+			"x2":0 + prmObj.xXlate,
+			"y2":0 + prmObj.yXlate + prmObj.length
+		});
+	}else if(prmObj.align == "y-right"){
+		ln.attr({
+			"x2":0 + prmObj.xXlate,
+			"y2":0 + prmObj.yXlate + prmObj.length
 		});
 	}else{
 		ln.attr({
@@ -505,22 +551,92 @@ function a0050(svgElmnt, prmObj){
 	//<nn>
 	// A tick-ekhez tartozó line elemeket generáljuk le.
 	//</nn>
-	for(var ix1=0; ix1<prmObj.ticks.tckNr; ix1++){
-		var tLn = $(document.createElementNS(PSTCG_CNSTS.SVGNS, "line"));
-		tLn.attr({
-			"id":"tck-"+ix1,
-			"x1":0 + prmObj.xXlate + ((ix1 + 1) * step),
-			"y1":prmObj.yXlate,
-			"x2":0 + prmObj.xXlate + ((ix1 + 1) * step),
-			"y2":prmObj.yXlate + prmObj.ticks.length,
-			"stroke":prmObj.ticks.lineColor,
-			"stroke-width":1,
-			"opacity":1
-		});
-		tckGrp.append(tLn);
+	if(prmObj.align == "x-bottom"){
+		for(var ix1=0; ix1<prmObj.ticks.tckNr; ix1++){
+			var tLn = $(document.createElementNS(PSTCG_CNSTS.SVGNS, "line"));
+			tLn.attr({
+				"id":"tck-x-"+ix1,
+				"x1":0 + prmObj.xXlate + ((ix1 + 1) * step),
+				"y1":prmObj.yXlate,
+				"x2":0 + prmObj.xXlate + ((ix1 + 1) * step),
+				"y2":prmObj.yXlate + prmObj.ticks.length,
+				"stroke":prmObj.ticks.lineColor,
+				"stroke-width":1,
+				"opacity":1
+			});
+			tckGrp.append(tLn);
+			//<nn>
+			// Ha a szöveghozzáadás kell, akkor betesszük szöveget is a tenegly-tick-ekhez
+			//</nn>
+			if(prmObj.showTickText){
+				var txt = $(document.createElementNS(PSTCG_CNSTS.SVGNS, "text"));
+				txt.attr({
+					"id":"tck-txt-x-"+ix1,
+					"x":0 + prmObj.xXlate + ((ix1 + 1) * step) - 2,
+					"y":prmObj.yXlate + 12,
+					"font-family":"Verdana",
+					"font-size":PSTCG_CNSTS.DEF_AX_TCK_FONT_SIZE,
+					"fill":"#050505",
+					"opacity":1
+				});
+				txt.text(prmObj.tickText[ix1]);
+				tckGrp.append(txt);
+			}
+		}
+	}else if(prmObj.align == "y-left"){
+		for(var ix1=0; ix1<prmObj.ticks.tckNr; ix1++){
+			var tLn = $(document.createElementNS(PSTCG_CNSTS.SVGNS, "line"));
+			tLn.attr({
+				"id":"tck-y-"+ix1,
+				"x1":0 + prmObj.xXlate,
+				"y1":0 + prmObj.yXlate + ((ix1 + 1) * step),
+				"x2":0 + prmObj.xXlate - prmObj.ticks.length,
+				"y2":0 + prmObj.yXlate + ((ix1 + 1) * step),
+				"stroke":prmObj.ticks.lineColor,
+				"stroke-width":1,
+				"opacity":1
+			});
+			tckGrp.append(tLn);
+			//<nn>
+			// Ha a szöveghozzáadás kell, akkor betesszük szöveget is a tenegly-tick-ekhez
+			//</nn>
+			if(prmObj.showTickText){
+				var txt = $(document.createElementNS(PSTCG_CNSTS.SVGNS, "text"));
+				txt.attr({
+					"id":"tck-txt-x-"+ix1,
+					"x":0 + prmObj.xXlate - 10,
+					"y":0 + prmObj.yXlate + ((ix1 + 1) * step) + 3,
+					"font-family":"Verdana",
+					"font-size":PSTCG_CNSTS.DEF_AX_TCK_FONT_SIZE,
+					"fill":"#050505",
+					"opacity":1
+				});
+				txt.text(prmObj.tickText[ix1]);
+				tckGrp.append(txt);
+			}
+		}
+	}else if(prmObj.align == "y-right"){
+		for(var ix1=0; ix1<prmObj.ticks.tckNr; ix1++){
+			var tLn = $(document.createElementNS(PSTCG_CNSTS.SVGNS, "line"));
+			tLn.attr({
+				"id":"tck-y-"+ix1,
+				"x1":0 + prmObj.xXlate,
+				"y1":0 + prmObj.yXlate + ((ix1 + 1) * step),
+				"x2":0 + prmObj.xXlate + prmObj.ticks.length,
+				"y2":0 + prmObj.yXlate + ((ix1 + 1) * step),
+				"stroke":prmObj.ticks.lineColor,
+				"stroke-width":1,
+				"opacity":1
+			});
+			tckGrp.append(tLn);
+		}
 	}
+	
+	
+	
+	
 	//<nn>
-	// A tengely GROUP-jához adjuk a tivk-groupot is.
+	// A tengely GROUP-jához adjuk a tick-groupot is.
 	//</nn>
 	axsGrp.append(tckGrp);
 	
@@ -531,7 +647,77 @@ function a0050(svgElmnt, prmObj){
 	
 }
 
-
+function a0060(sclMin, sclMax, domMin, domMax){
+//<SF>
+// 2018. ápr. 24.<br>
+// A skála elemet generáló függvény.<br>
+// Ez lenne az a függvény, ami az adatok, és a grafikon terjedelméből/mérteéből, az egyes adatelemeket
+// koordináta értékekre fordítja le.
+// Ehhez persze tudni akell, hogy mi lesz az értelmezés tartaomány (adatok terjedelme), és mi lesz az értékkészlet (grafikon adott dimenziója)
+// PARAMÉTEREK:
+//×-
+// @-- @param sclMin = a grafikon minimumértéke (0) -@
+// @-- @param sclMax = a grafikon maximumértéke (0) -@
+// @-- @param domMin = az adatok minimumértéke (0) -@
+// @-- @param domMax = az adatok maximumértéke (0) -@
+//-×
+//MÓDOSÍTÁSOK:
+//×-
+// @-- ... -@
+//-×
+//</SF>
+	
+	if(sclMin == undefined){
+		sclMin = 0;
+	}
+	if(sclMax == undefined){
+		sclMax = 330;
+	}
+	if(domMin == undefined){
+		domMin = 50;
+	}
+	if(domMax == undefined){
+		domMax = 1000;
+	}
+	
+	//<nn>
+	// A visszadandó objektum, aminek a getVal függvényét használjuk majd a skálázáshoz, 
+	// amivel az ábrázolandó értékeket átkonvertáljuk pixelbeni távolságokká.
+	//</nn>
+	var scl = {};
+	
+	//<nn>
+	// A scale objektum elemei:
+	// min/maxVAL => az értékkészlet maxi/mini-muma, vagyis a pixlekoordináták <br>
+	// min/maxDOM => az értelmezési tartomány maxi/mini-muma, vagyis az adatok kerekített szélsőértékei <br> 
+	// extVal/Dom => az értékkészlet, és az értelmezési tartomány terjedelme.
+	//
+	//</nn>
+	scl.minVal = sclMin;
+	scl.maxVal = sclMax;
+	scl.minDom = domMin;
+	scl.maxDom = domMax;
+	scl.extVal = scl.maxVal - scl.minVal;
+	scl.extDom = scl.maxDom - scl.minDom;
+	
+	scl.getVal = function(val){
+		
+		if(val >= this.maxDom){
+			return this.maxVal;
+		}else if(val <= this.minDom){
+			return this.minVal;
+		}else{
+			console.log("this.maxVal-this.minVal: " + (this.maxVal-this.minVal));
+			console.log("this.maxDom-this.minDom: " + (this.maxDom-this.minDom));
+			return (val - this.minDom) * (this.extVal/this.extDom);
+		}
+	};
+	
+	//<nn>
+	// A scale objektumunkat viszaadjuk.
+	//</nn>
+	return scl;
+}
 
 
 
